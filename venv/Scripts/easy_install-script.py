8566d8d7d98ff4a56e5ea6646babcce6e46d3e4b
nana <d16124657@mydit.ie>
2018-12-23 14:05:13 +0000

    data preparing

---
+++ venv/Scripts/easy_install-script.py
@@ -0,0 +1,12 @@
+#!D:\DIT-2016-2019\NEW\Dissertation\CP-in-TCI\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==39.1.0','console_scripts','easy_install'
+__requires__ = 'setuptools==39.1.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==39.1.0', 'console_scripts', 'easy_install')()
+    )
